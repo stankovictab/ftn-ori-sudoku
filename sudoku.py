62a1e0db263b13ac2e4f658e12de96957d44f3d3
stankovictab <stankovictab@gmail.com>
2021-05-28 16:49:06 +0200

    refactor: trivialFill()

--- sudoku.py
+++ sudoku.py
@@ -9,33 +9,33 @@ GTPARtestPuzzle = []
 
 # 0 su prazna mesta
 # 1 - Easy - 0.3s na DualCore sa printovima
-# puzzle = [
-# 	[8,7,4, 1,9,2, 0,0,3],
-# 	[2,3,0, 0,7,0, 8,1,9],
-# 	[6,1,9, 3,0,8, 4,7,2],
-
-# 	[0,0,2, 7,0,5, 0,8,0],
-# 	[7,0,0, 0,1,0, 0,2,5],
-# 	[0,5,0, 0,2,0, 7,0,0],
-
-# 	[9,6,7, 5,0,1, 2,4,8],
-# 	[0,8,0, 2,4,7, 0,9,0],
-# 	[4,2,1, 9,8,0, 5,0,7]
-# ]
-# 2 - Medium - 0.6s na DualCore sa printovima
 puzzle = [
-	[3,8,9, 2,7,0, 6,0,4],
-	[7,0,6, 9,1,0, 2,8,5],
-	[0,0,0, 8,4,0, 0,3,0],
+	[8,7,4, 1,9,2, 0,0,3],
+	[2,3,0, 0,7,0, 8,1,9],
+	[6,1,9, 3,0,8, 4,7,2],
 
-	[6,0,4, 3,0,2, 0,0,0],
-	[0,1,8, 0,0,7, 0,0,2],
-	[0,0,0, 6,0,0, 0,5,0],
+	[0,0,2, 7,0,5, 0,8,0],
+	[7,0,0, 0,1,0, 0,2,5],
+	[0,5,0, 0,2,0, 7,0,0],
 
-	[0,0,0, 7,0,0, 1,0,6],
-	[1,0,0, 0,0,8, 0,0,0],
-	[0,6,3, 0,0,0, 5,7,0]
+	[9,6,7, 5,0,1, 2,4,8],
+	[0,8,0, 2,4,7, 0,9,0],
+	[4,2,1, 9,8,0, 5,0,7]
 ]
+# 2 # - Medium - 0.6s na DualCore sa printovima
+# puzzle = [
+# 	[3,8,9, 2,7,0, 6,0,4],
+# 	[7,0,6, 9,1,0, 2,8,5],
+# 	[0,0,0, 8,4,0, 0,3,0],
+
+# 	[6,0,4, 3,0,2, 0,0,0],
+# 	[0,1,8, 0,0,7, 0,0,2],
+# 	[0,0,0, 6,0,0, 0,5,0],
+
+# 	[0,0,0, 7,0,0, 1,0,6],
+# 	[1,0,0, 0,0,8, 0,0,0],
+# 	[0,6,3, 0,0,0, 5,7,0]
+# ]
 # 3 - Expert - 5min na DualCore sa printovima, 64s na QuadCore bez printova
 # puzzle = [
 # 	[0,0,0, 7,0,0, 5,0,1],
@@ -67,112 +67,48 @@ puzzle = [
 
 # Program radi za 9x9 sudoku, ako hocemo 6x6 ili 12x12 moramo check-u da prosledimo i dimenziju da zna kako da radi
 
-# TODO:
-###########################################################################
-# def checkAtPos(puzzle, r, c):
-#         selected = puzzle[r][c] # r i c su indeksi selektovanog elementa
-#         if(selected != 0):
-#             # Provera po koloni, inkrementuje se row
-#             for i in range(9):
-#                 if(i == r):
-#                     continue
-#                 if(selected == puzzle[i][c]): # i != r znaci da preskace indeks selektovanog
-#                     return False
-#             # Provera po redu, inkrementuje se col
-#             for i in range(9):
-#                 if(i == c):
-#                     continue
-#                 if(selected == puzzle[r][i]):
-#                     return False
+def checkAtPos(puzzle, r, c):
+		selected = puzzle[r][c] # r i c su indeksi selektovanog elementa
+		if(selected != 0):
+			# Provera po koloni, inkrementuje se row
+			for i in range(9):
+				if(i == r):
+					continue
+				if(selected == puzzle[i][c]): # i != r znaci da preskace indeks selektovanog
+					return False
+			# Provera po redu, inkrementuje se col
+			for i in range(9):
+				if(i == c):
+					continue
+				if(selected == puzzle[r][i]):
+					return False
+			
+			r33 = math.floor(r / 3)
+			c33 = math.floor(c / 3)
+			r33coordinates = [r33 * 3, r33 * 3 + 1, r33 * 3 + 2]
+			c33coordinates = [c33 * 3, c33 * 3 + 1, c33 * 3 + 2]
 
-#             r33 = math.floor(r / 3)
-#             c33 = math.floor(c / 3)
-#             r33coordinates = [0,0,0] # niz u koji smestamo indekse kolona grida u kom se nalazi trazeni broj
-#             r33coordinates[0] = r33 * 3
-#             r33coordinates[1] = r33 * 3 + 1
-#             r33coordinates[2] = r33 * 3 + 2
-#             c33coordinates = [0,0,0]
-#             c33coordinates[0] = c33 * 3
-#             c33coordinates[1] = c33 * 3 + 1
-#             c33coordinates[2] = c33 * 3 + 2
-
-#             for i in range(3): # od 0 do 2
-#                 for j in range(3):
-#                     # Preskace selektovani
-#                     if(r == r33coordinates[i] and c == c33coordinates[j]): # and mora, ne &
-#                         continue
-#                     # Ako nadje broj u sekciji koji je isti kao selektovani
-#                     if(puzzle[r33coordinates[i]][c33coordinates[j]] == selected): 
-#                         return False
-#             return True
-# # Ovo govori samo da li je sve u sudokuu uredu, ne da li moze da se resi (za sve nule ce reci da je ok)
-# # Reci ce da ako ima dva ista u koloni ili redu ili 3x3 da nije dobro
-# def check(puzzle):
-#     # Za svaki element gleda svoj red, kolonu i 3x3
-#     for r in range(9): # od 0 do 8
-#         for c in range(9):
-#             if checkAtPos(puzzle, r, c) == False:
-#                 return False
-#     return True
-#############################################################################
+			for i in range(3): # od 0 do 2
+				for j in range(3):
+					# Preskace selektovani
+					if(r == r33coordinates[i] and c == c33coordinates[j]): # and mora, ne &
+						continue
+					# Ako nadje broj u sekciji koji je isti kao selektovani
+					if(puzzle[r33coordinates[i]][c33coordinates[j]] == selected): 
+						return False
+			return True
 
 # Ovo govori samo da li je sve u sudokuu uredu, ne da li moze da se resi (za sve nule ce reci da je ok)
 # Reci ce da ako ima dva ista u koloni ili redu ili 3x3 da nije dobro
 def check(puzzle):
 	# Za svaki element gleda svoj red, kolonu i 3x3
-	for r in range(9): # od 0 do 8   
+	for r in range(9): # od 0 do 8
 		for c in range(9):
-			selected = puzzle[r][c] # r i c su indeksi selektovanog elementa
-			# print(selected)
-			if(selected != 0):
-				# Provera po koloni, inkrementuje se row
-				for i in range(9):
-					if(i == r):
-						continue
-					if(selected == puzzle[i][c]): # i != r znaci da preskace indeks selektovanog
-						return False
-				# Provera po redu, inkrementuje se col
-				for i in range(9):
-					if(i == c):
-						continue
-					if(selected == puzzle[r][i]):
-						return False
-				# Provera po 3x3
-				# Treba da nadje u kom je segmentu po modulu, i po row i po col, pa da proveri sve u tom segmentu osim samog selektovanog
-				# print("R = " + str(r) + "   R mod 3 = " + str(math.floor(r / 3)))
-				# print("C = " + str(c) + "   C mod 3 = " + str(math.floor(c / 3)))
-				# Koordinate sekcije, moze biti 0, 1 ili 2
-				r33 = math.floor(r / 3)
-				c33 = math.floor(c / 3)
-				r33coordinates = [0,0,0] # niz u koji smestamo indekse kolona grida u kom se nalazi trazeni broj
-				r33coordinates[0] = r33 * 3
-				r33coordinates[1] = r33 * 3 + 1
-				r33coordinates[2] = r33 * 3 + 2
-				c33coordinates = [0,0,0]
-				c33coordinates[0] = c33 * 3
-				c33coordinates[1] = c33 * 3 + 1
-				c33coordinates[2] = c33 * 3 + 2
-				
-				# for i in r33coordinates:
-				# 	print("R33 Element: " + str(i))
-				# for i in c33coordinates:
-				# 	print("C33 Element: " + str(i))
-
-				for i in range(3): # od 0 do 2
-					for j in range(3):
-						# Preskace selektovani
-						if(r == r33coordinates[i] and c == c33coordinates[j]): # and mora, ne &
-							# print("R33C[i] = " + str(r33coordinates[i]) + "   C33C[j] = " + str(c33coordinates[j]))
-							# print("puzzle[r33i,c33j] = " + str(puzzle[r33coordinates[i]][c33coordinates[j]]))
-							continue
-						# Ako nadje broj u sekciji koji je isti kao selektovani
-						if(puzzle[r33coordinates[i]][c33coordinates[j]] == selected): 
-							# print("R33C[i] = " + str(r33coordinates[i]) + "   C33C[j] = " + str(c33coordinates[j]))
-							# print("puzzle[r33i,c33j] = " + str(puzzle[r33coordinates[i]][c33coordinates[j]]))
-							return False
+			if checkAtPos(puzzle, r, c) == False:
+				return False
 	return True
 
-def fillOneEmpty(puzzle):
+def trivialFill(puzzle):
 	# Svaka globalna promenljiva koja se koristi mora da ima global imePromenljive na pocetku funkcije
 	global flag
 	flag = 0 # Promena globalne promenljive
@@ -195,7 +131,9 @@ def fillOneEmpty(puzzle):
 		else:
 			flag = 1
 			puzzle[zeroPosition[0]][zeroPosition[1]] = numbersList[0]
-			print("Inserted " + str(numbersList[0]) + " into the puzzle on (" + str(zeroPosition[0]) + "," + str(zeroPosition[1]) + ") by rows.")
+			positionStack.append((zeroPosition[0], zeroPosition[1]))
+			dict[(zeroPosition[0], zeroPosition[1])] = []
+			print("Inserted " + str(numbersList[0]) + " into the puzzle on (" + str(zeroPosition[0]) + "," + str(zeroPosition[1]) + ") by trivialFill Row.")
 	
 	# Provera za kolone
 	for j in range(9):
@@ -214,7 +152,9 @@ def fillOneEmpty(puzzle):
 		else:
 			flag = 1
 			puzzle[zeroPosition[0]][zeroPosition[1]] = numbersList[0]
-			print("Inserted " + str(numbersList[0]) + " into the puzzle on (" + str(zeroPosition[0]) + "," + str(zeroPosition[1]) + ") by columns.")
+			positionStack.append((zeroPosition[0], zeroPosition[1]))
+			dict[(zeroPosition[0], zeroPosition[1])] = []
+			print("Inserted " + str(numbersList[0]) + " into the puzzle on (" + str(zeroPosition[0]) + "," + str(zeroPosition[1]) + ") by trivialFill Col.")
 
 	# Provera za 3x3
 	for sectionNum in range(9):
@@ -237,7 +177,9 @@ def fillOneEmpty(puzzle):
 		else:
 			flag = 1
 			puzzle[zeroPosition[0]][zeroPosition[1]] = numbersList[0]
-			print("Inserted " + str(numbersList[0]) + " into the puzzle on (" + str(zeroPosition[0]) + "," + str(zeroPosition[1]) + ") by 3x3.")
+			positionStack.append((zeroPosition[0], zeroPosition[1]))
+			dict[(zeroPosition[0], zeroPosition[1])] = []
+			print("Inserted " + str(numbersList[0]) + " into the puzzle on (" + str(zeroPosition[0]) + "," + str(zeroPosition[1]) + ") by trivialFill 3x3.")
 
 # Trazi redom koje je prvo mesto matrice koje je prazno, za backtracking
 def findZero(puzzle):
@@ -257,20 +199,15 @@ def fillFirstZero(puzzle):
 	for i in range(9):
 		if puzzle[x][i] != 0 and puzzle[x][i] in dict[(x,y)]:
 			dict[(x,y)].remove(puzzle[x][i])
-	print("Ostatak elemenata na (" + str(x) + "," + str(y) + ") po fillFirstZero po redu je : " + str(dict[(x,y)]))
-	# Nikad nece biti len(numbersList) == 1 zbog fillOneEmpty()
+	print("Remaining elements on (" + str(x) + "," + str(y) + ") by fillFirstZero Row are : " + str(dict[(x,y)]))
+	# Nikad nece biti len(numbersList) == 1 zbog trivialFill()
 
 	# Provera po koloni
 	for j in range(9):
 		if puzzle[j][y] != 0 and puzzle[j][y] in dict[(x,y)]:
 			dict[(x,y)].remove(puzzle[j][y])
-	print("Ostatak elemenata na (" + str(x) + "," + str(y) + ") po fillFirstZero po koloni je : " + str(dict[(x,y)]))
-	# GRESKA - NE MOZE DA STAVLJA PO COL AKO NE PROVERI 3X3
-	# if len(dict[(x,y)]) == 1:
-	# 	puzzle[x][y] = dict[(x,y)][0] # Da prekrati muke
-	# 	positionStack.append((x,y))
-	# 	print("Inserted " + str(dict[(x,y)][0]) + " into the puzzle on (" + str(x) + "," + str(y) + ") by backtracking COL.")
-	# 	return "ideal"
+	print("Remaining elements on (" + str(x) + "," + str(y) + ") po fillFirstZero Col are : " + str(dict[(x,y)]))
+	# Iako ima 1 preostao element, on ne moze da se doda a da se ne proveri 3x3
 
 	# Provera po 3x3
 	sectionX = math.floor(x / 3) # elemX = sectionX * 3
@@ -287,7 +224,7 @@ def fillFirstZero(puzzle):
 		yNew = yOffset + 3 * (sectionNum % 3)
 		if puzzle[xNew][yNew] != 0 and puzzle[xNew][yNew] in dict[(x,y)]:
 			dict[(x,y)].remove(puzzle[xNew][yNew])
-	print("Ostatak elemenata na (" + str(x) + "," + str(y) + ") po fillFirstZero po 3x3 je : " + str(dict[(x,y)]))
+	print("Remaining elements on (" + str(x) + "," + str(y) + ") by fillFirstZero 3x3 are : " + str(dict[(x,y)]))
 	for elemNum in range(9):
 		# print("Section: " + str(sectionNum) + " Elem: " + str(elemNum))
 		xOffset = math.floor(elemNum / 3)
@@ -306,10 +243,10 @@ def fillFirstZero(puzzle):
 				print("EMPTY DICT RETURNED Y: " + str(y))
 				return ("emptyDict", x, y, xNew, yNew)
 			testPuzzle[xNew][yNew] = dict[(x,y)][0]
-			if check(testPuzzle) == True:
+			if checkAtPos(testPuzzle, xNew, yNew): # Optimizacija, ne gleda check(testPuzzle)
 				puzzle[xNew][yNew] = dict[(x,y)][0]
 				positionStack.append((x,y)) # Stavlja na kraj
-				print("Inserted " + str(dict[(x,y)][0]) + " into the puzzle on (" + str(x) + "," + str(y) + ") by backtracking 3X3.")
+				print("Inserted " + str(dict[(x,y)][0]) + " into the puzzle on (" + str(x) + "," + str(y) + ") by fillFirstZero 3x3.")
 				dict[(x,y)].pop(0)
 				return ("tried", x, y, xNew, yNew) # Posle ovoga postavi globalni flag na 1 da zna da su svi sledeci elementi isto probni, i da pozicije treba da im budu na steku, apendovane
 			else:
@@ -321,50 +258,29 @@ def fillFirstZero(puzzle):
 				return ("failedCheck", x, y, xNew, yNew)
 
 def goToPreviousAndReplace(oldX, oldY):
-	global GTPARtestPuzzle
-
-	# testPos, testStatus = positionStack.pop() # Pritom i skida sa steka
-	# x,y = testPos
-	# while testStatus == "ideal":
-	# 	x, y = testPos
-	# 	puzzle[x][y] = 0
-	# 	print("Inserted 0 into the puzzle on (" + str(x) + "," + str(y) + ") by GTPAR WHILE.")
-	# 	testPos, testStatus = positionStack.pop()
-
+	global GTPARtestPuzzle # Globalni puzzle koji se koristi kod rekurzije
 	(x,y) = positionStack.pop() # Uzima sa kraja
-	# Da li moze da bude prazna lista u dict-u kad udje ovde?
 	GTPARtestPuzzle = copy.deepcopy(puzzle) # testPuzzle je lokalan za svaki poziv funkcije, sto nije dobro kod rekurzije
 	if len(dict[(x,y)]) == 0:
-		print("F in the chat")
 		puzzle[x][y] = 0
 		GTPARtestPuzzle[x][y] = 0 
-		# (xOld, yOld) = positionStack.pop()
-		goToPreviousAndReplace(x, y) 
-		# Kad se vrati iz poziva rekurzije, refresh-uje GTPARtestPuzzle, iako se u samom rekurzivnom pozivu on menja
-		# Zbog toga treba :
-		# GTPARtestPuzzle[x][y] = 0 
-		# Nije, nisu dobri x i y i treba 9 umesto 0
+		goToPreviousAndReplace(x, y)
 	else:
 		GTPARtestPuzzle[x][y] = dict[(x,y)][0]
-		# return?
-	if check(GTPARtestPuzzle) == True: # Bitno je da gleda globalne
+	if check(GTPARtestPuzzle) == True: # Bitno je da gleda globalne, ne moze checkAtPos(gtpartp, x, y)
 		if len(dict[(x,y)]) == 0:
 			return
 		puzzle[x][y] = dict[(x,y)][0]
 		GTPARtestPuzzle[x][y] = dict[(x,y)][0] # Zbog rekurzije, mozda nebitan
 		positionStack.append((x,y))
-		print("Inserted " + str(dict[(x,y)][0]) + " into the puzzle on (" + str(x) + "," + str(y) + ") by Previous and Replace.")
+		print("Inserted " + str(dict[(x,y)][0]) + " into the puzzle on (" + str(x) + "," + str(y) + ") by backtracking.")
 		dict[(x,y)].pop(0)
 	else:
-		print("DJOKEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-		print("Old X: " + str(oldX))
-		print("Old Y: " + str(oldY))
 		puzzle[oldX][oldY] = 0
 		GTPARtestPuzzle[x][y] = 0 # Zbog rekurzije
-		print("Inserted 0 into the puzzle on (" + str(oldX) + "," + str(oldY) + ") by GOTOPREVIOUS ELSE.")
+		print("Resetted 0 into the puzzle on (" + str(oldX) + "," + str(oldY) + ") for recursive backtracking.")
 		# Rekurzija
 		goToPreviousAndReplace(x, y)
-		
 
 def solve(puzzle):
 	global flag
@@ -377,29 +293,24 @@ def solve(puzzle):
 		# Nastavak rada
 		# Gleda da li moze trivijalno ubacivanje jednog elementa da uradi, sto se radi po flag-u
 		# Kada ne moze vise, radi fillFirstZero koji ubacuje prvi element iz numbersListe u dictu
-		while flag == 1:
-			fillOneEmpty(puzzle)
-		sign = fillFirstZero(puzzle)
 
 		# Tu lezi zec, ovo se poziva samo kada smo 100% sigurni da po fillFirstZero treba da se upise neki broj, to ce biti ili kad se radi po COL pa znamo da je ostao samo jedan element u onoj listi, ili u 3x3 kada dodjemo do tog poslednjeg elementa (to je idealan scenario)
-		# Ako se ne radi tako, ako pozivamo fillOneEmpty svaki put kada ubacimo bilo koj element iz liste, to ce pogorsati stvari jer necemo znati koje sve elemente da brisemo kada radimo backtracking, imacemo vise elemenata zbog tog fillOneEmpty
-		if sign == "ideal":
+		# Ako se ne radi tako, ako pozivamo trivialFill() svaki put kada ubacimo bilo koj element iz liste, to ce pogorsati stvari jer necemo znati koje sve elemente da brisemo kada radimo backtracking, imacemo vise elemenata zbog tog trivialFill()
+		while True:
 			flag = 1
 			while flag == 1:
-				fillOneEmpty(puzzle) # TODO: Da li ovo moze da se ponavlja u while-u? Treba nam rad sa stekom i sa ovim brojevima koji se dodaju, da se i oni vracaju uz backtrack
-		while check(puzzle) == True: # TODO: check() nije potreban svaki put da se radi, moze da se radi samo za dodati element, a mozda cak nije uopste potreban zbog toga kako radi fillFirstZero?
+				trivialFill(puzzle) # Treba nam rad sa stekom i sa ovim brojevima koji se dodaju, da se i oni vracaju uz backtrack
 			for i in range(9):
 				print(puzzle[i])
 			returnElements = fillFirstZero(puzzle)
 			if returnElements == None:
-				print("Izasao je iz while-a na dobar nacin, nema vise nula u Sudoku-u")
+				print("Nema vise nula u Sudoku-u!")
 				return
 			status = returnElements[0]
 			if status == "emptyDict" or status == "failedCheck":
 				x = returnElements[1]
 				y = returnElements[2]
 				goToPreviousAndReplace(x, y)
-		print("Pogresno je izasao iz while-a - check() nije dobar")
 
 startTime = time.time()
 solve(puzzle)
